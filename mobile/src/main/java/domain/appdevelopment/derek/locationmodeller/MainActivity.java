30774ec9eb08e0a062bfd3add7dd99d6fa02f120
Your Name <you@example.com>
2018-03-30 03:42:37 +0800

    Updating customized display for modeller.

--- mobile/src/main/java/domain/appdevelopment/derek/locationmodeller/MainActivity.java
+++ mobile/src/main/java/domain/appdevelopment/derek/locationmodeller/MainActivity.java
@@ -2,6 +2,7 @@ package domain.appdevelopment.derek.locationmodeller;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
@@ -9,6 +10,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,8 +25,10 @@ import java.util.List;
 
 import domain.appdevelopment.derek.locationmodeller.db.DBHandler;
 import domain.appdevelopment.derek.locationmodeller.entity_relationship.LocationStay;
+import domain.appdevelopment.derek.locationmodeller.entity_relationship.Place;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     private static final int GPS_PERMISSION = 0;
     /**
      * The google location service provider.
@@ -40,12 +46,52 @@ public class MainActivity extends AppCompatActivity {
      * The database that stores the location information.
      */
     private DBHandler db;
+    /**
+     * The text view for display
+     */
+    private Button loc_dis;
+    /**
+     * The button to control the display of collected LocationStays.
+     */
+    private Button stay_dis;
+    /**
+     * Intent for displaying collected information.
+     */
+    private Intent display;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = new DBHandler(this);
+        //Fetch the buttons for display.
+        loc_dis = findViewById(R.id.place_display);
+        stay_dis = findViewById(R.id.stay_display);
+        loc_dis.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                display = new Intent(MainActivity.this,DisplayTable.class);
+                List<Place> places = db.getAllPlaces();
+                StringBuilder builder = new StringBuilder();
+                for(Place pla: places)
+                    builder.append(pla.toString()).append("\n");
+                display.putExtra("table",builder.toString());
+                startActivity(display);
+            }
+        });
+        stay_dis.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                display = new Intent(MainActivity.this,DisplayTable.class);
+                List<LocationStay> stays = db.getCurrentStays();
+                String dis = stays.toString().replaceAll(",","\n");
+                display.putExtra("table",dis);
+                startActivity(display);
+            }
+        });
         //Ask for gps permission.
         if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, GPS_PERMISSION);
@@ -59,8 +105,6 @@ public class MainActivity extends AppCompatActivity {
                     else {
                         Location currentLoc = result.getLocations().get(0);
                         db.updateStay(currentLoc.getTime(), currentLoc.getLatitude(), currentLoc.getLongitude());
-                        List<LocationStay> stays = db.getCurrentStays();
-                        Toast.makeText(MainActivity.this,stays.get(stays.size()-1).toString(),Toast.LENGTH_SHORT).show();
                     }
                 }
             };
@@ -85,8 +129,6 @@ public class MainActivity extends AppCompatActivity {
                         else {
                             Location currentLoc = result.getLocations().get(0);
                             db.updateStay(currentLoc.getTime(), currentLoc.getLatitude(), currentLoc.getLongitude());
-                            List<LocationStay> stays = db.getCurrentStays();
-                            Toast.makeText(MainActivity.this,stays.get(stays.size()-1).toString(),Toast.LENGTH_SHORT).show();
                         }
                     }
                 };
@@ -100,12 +142,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Terminate the location processing.
-     */
-    protected void onPause() {
-        super.onPause();
-        //client.removeLocationUpdates(callback);
-    }
+   protected void onDestroy()
+   {
+       super.onDestroy();
+       client.removeLocationUpdates(callback);
+   }
 
 }
